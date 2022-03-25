c8c5fb7b3924820f84bfc42f641bf5dd2108b6d7
KarthikShetty11 <kvshetty27@gmail.com>
2022-03-25 21:32:18 +0530

    dicegame completed

---
+++ diceGame/index.js
@@ -0,0 +1,78 @@
+
+/*
+var diceRoll = Math.floor(Math.random()*6)+1;
+
+if( diceRoll === 1)
+{
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice1.png");
+}
+else if( diceRoll === 2)
+{
+
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice2.png");
+}
+else if( diceRoll === 3)
+{
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice3.png");
+}
+else if( diceRoll === 4)
+{
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice4.png");
+}
+else if( diceRoll === 5)
+{
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice5.png");
+}
+else
+{
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice6.png");
+}
+
+var diceRoll2 = Math.floor(Math.random()*6)+1;
+
+if(diceRoll2 === 1)
+{
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice1.png");
+}
+else if(diceRoll2 === 2)
+{
+
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice2.png");
+}
+else if(diceRoll2 === 3)
+{
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice3.png");
+}
+else if(diceRoll2 === 4)
+{
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice4.png");
+}
+else if(diceRoll2 === 5)
+{
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice5.png");
+}
+else
+{
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice6.png");
+}
+*/
+
+var diceRoll = Math.floor(Math.random()*6)+1;
+var newImg = "images/dice"+diceRoll+".png";
+document.querySelector(".img1").setAttribute("src", newImg);   
+
+var diceRoll2 = Math.floor(Math.random()*6)+1;
+var newImg2 = "images/dice"+diceRoll2+".png";
+document.querySelector(".img2").setAttribute("src",newImg2);  
+
+if ( diceRoll === diceRoll2)
+{
+    document.querySelector("h1").textContent="Its a Draw";
+}
+else if(diceRoll < diceRoll2){
+    document.querySelector("h1").textContent ="Player 2 is the Winner";
+}
+else
+{
+    document.querySelector("h1").textContent="Player 1 is the Winner";
+}
